6edeb8c447ca8ae8f513dcd03279c03f0fd2c590
adarshrs97 <truthrsak@gmail.com>
2020-07-21 23:46:13 +0530

    host 0.0.0.0 added

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def querycomb(query,replacement):
     # open('qbout.txt', 'w').close()
 
 if __name__ == '__main__':
-    app.run(port=5662, debug=True)
+    app.run(host='0.0.0.0',port=5662, debug=True)
 
